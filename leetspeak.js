fb384686fe64bf5b3e8716b2efdeca50c733813c
Kelli <kellistone217@gmail.com>
2021-12-28 19:36:45 -0700

    Added the exercise instructions

--- leetspeak.js
+++ leetspeak.js
@@ -1,3 +1,7 @@
+//Write a function leetspeak which is given a string, and returns the leetspeak equivalent of the string. To convert text to its leetspeak version, make the following substitutions:
+
+// A => 4 E => 3 G => 6 I => 1 O => 0 S => 5 T => 7
+
 function leetspeak(thisString) {
     // if string contains the following letters, print the corresponding numbers
     // a = 4, e = 3, g = 6, i = 1, o = 0, s = 5, t = 7
